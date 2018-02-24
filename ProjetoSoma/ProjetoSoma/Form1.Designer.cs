477f2168a054aee580a160e8d2f40ca8e3f48653
Andrew <33047131+AndrewLodi@users.noreply.github.com>
2018-02-23 21:37:00 -0300

    Calculadora

---
+++ ProjetoSoma/ProjetoSoma/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ProjetoSoma
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txbPrimeiroNumero = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.txbSegundoNumero = new System.Windows.Forms.TextBox();
+            this.lblSegundoNumero = new System.Windows.Forms.Label();
+            this.lblPrimeiroNumero = new System.Windows.Forms.Label();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbPrimeiroNumero
+            // 
+            this.txbPrimeiroNumero.Location = new System.Drawing.Point(8, 59);
+            this.txbPrimeiroNumero.Name = "txbPrimeiroNumero";
+            this.txbPrimeiroNumero.Size = new System.Drawing.Size(199, 20);
+            this.txbPrimeiroNumero.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 261);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(15, 182);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(93, 23);
+            this.btnSomar.TabIndex = 5;
+            this.btnSomar.Text = "+";
+            this.toolTip1.SetToolTip(this.btnSomar, "Clique neste botão para somar.");
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txbSegundoNumero
+            // 
+            this.txbSegundoNumero.Location = new System.Drawing.Point(8, 128);
+            this.txbSegundoNumero.Name = "txbSegundoNumero";
+            this.txbSegundoNumero.Size = new System.Drawing.Size(199, 20);
+            this.txbSegundoNumero.TabIndex = 3;
+            // 
+            // lblSegundoNumero
+            // 
+            this.lblSegundoNumero.AutoSize = true;
+            this.lblSegundoNumero.Location = new System.Drawing.Point(12, 112);
+            this.lblSegundoNumero.Name = "lblSegundoNumero";
+            this.lblSegundoNumero.Size = new System.Drawing.Size(130, 13);
+            this.lblSegundoNumero.TabIndex = 2;
+            this.lblSegundoNumero.Text = "Digite o segundo Número:";
+            // 
+            // lblPrimeiroNumero
+            // 
+            this.lblPrimeiroNumero.AutoSize = true;
+            this.lblPrimeiroNumero.Location = new System.Drawing.Point(12, 43);
+            this.lblPrimeiroNumero.Name = "lblPrimeiroNumero";
+            this.lblPrimeiroNumero.Size = new System.Drawing.Size(123, 13);
+            this.lblPrimeiroNumero.TabIndex = 0;
+            this.lblPrimeiroNumero.Text = "Digite o primeiro número:";
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Location = new System.Drawing.Point(114, 211);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(93, 23);
+            this.btnDivisao.TabIndex = 7;
+            this.btnDivisao.Text = "/";
+            this.toolTip1.SetToolTip(this.btnDivisao, "Clique neste botão para somar.");
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(15, 211);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(93, 23);
+            this.btnMultiplicar.TabIndex = 8;
+            this.btnMultiplicar.Text = "*";
+            this.toolTip1.SetToolTip(this.btnMultiplicar, "Clique neste botão para somar.");
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Location = new System.Drawing.Point(114, 182);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(93, 23);
+            this.btnSubtrair.TabIndex = 9;
+            this.btnSubtrair.Text = "-";
+            this.toolTip1.SetToolTip(this.btnSubtrair, "Clique neste botão para somar.");
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 312);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txbSegundoNumero);
+            this.Controls.Add(this.lblSegundoNumero);
+            this.Controls.Add(this.txbPrimeiroNumero);
+            this.Controls.Add(this.lblPrimeiroNumero);
+            this.Name = "frmPrincipal";
+            this.Text = "Somar dois números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txbPrimeiroNumero;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.TextBox txbSegundoNumero;
+        private System.Windows.Forms.Label lblSegundoNumero;
+        private System.Windows.Forms.Label lblPrimeiroNumero;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSubtrair;
+    }
+}
+
